8e3c6418ca2db036282acf1f352929917ad8935d
marcoguilherme <mgbm13@gmail.com>
2018-02-26 03:55:12 +0000

    Adicionado informacoes adicionais aos arquivos de rotas e configuracao de gems

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+
+  # Para configurar subdominio
+  # namespace :api, defaults: { format: 'json' }, constraints: { subdomain: 'api' }, path: "/" do
+  # exemplo: api.meudominio.com.br/usuarios
+
+  # Para acessar usando path
+  # namespace :api, defaults: { format: 'json' }  do
+  # exemplo: meudominio.com.br/api/usuarios
   
-  namespace :api, defaults: { format: json } do
+  namespace :api, defaults: { format: 'json' }  do
     
   end
 end
